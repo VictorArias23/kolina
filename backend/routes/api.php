8ee5d9b7d7fa8b632215309ea5a01e35120c116b
Lampar-2030 <ariasvictor254@gmail.com>
2026-09-08 23:04:42 -0500

    se modifica backend/routes/api y se agrega la ruta al controlador

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\VentaController; // -> se agrega nuevo controlador a la API
+
 
 Route::get('/test', function () {
     return response()->json([
@@ -14,4 +16,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+});
+
+// agregado por controlador VentaController
+Route::prefix('v1')->group(function () {
+    Route::apiResource('ventas', VentaController::class);
+    Route::patch('ventas/{id}/estado', [VentaController::class, 'cambiarEstado']);
 });
