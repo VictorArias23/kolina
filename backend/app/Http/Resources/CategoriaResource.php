f4de6ac0789cfbfd8cd8b6f4a6c3b6957e17c2c3
Lampar-2030 <ariasvictor254@gmail.com>
2026-09-09 00:39:48 -0500

    Creacion de Recurso - Categoria Resourse

---
+++ backend/app/Http/Resources/CategoriaResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoriaResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'nombre' => $this->nombre,
+            'codigo' => $this->codigo,
+            'descripcion' => $this->descripcion,
+            'color' => $this->color,
+            'icono' => $this->icono,
+            'activo' => $this->activo,
+            'total_productos' => $this->productos->count(),
+            'productos' => ProductoResource::collection($this->whenLoaded('productos')),
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
+        ];
+    }
+}
